9063688e56ea1feb5b1287a7012b25c79241d118
Nick Verlinden <nick@studiokitara.com>
2017-07-13 18:44:13 +0200

    Updated dependencies. Configuration is now allume profile aware.

--- bin/cc.config/config.js
+++ bin/cc.config/config.js
@@ -1,16 +1,16 @@
 /////////////////////////////////////////////////////////////////////////////////////
 //
-// module 'cc.config.0.1.13/'
+// module 'cc.config.0.1.14/'
 //
 /////////////////////////////////////////////////////////////////////////////////////
 (function(using, require) {
     define.parameters = {};
     define.parameters.wrapped = true;
     define.parameters.system = "pkx";
-    define.parameters.id = "cc.config.0.1.13/";
+    define.parameters.id = "cc.config.0.1.14/";
     define.parameters.pkx = {
         "name": "cc.config",
-        "version": "0.1.13",
+        "version": "0.1.14",
         "title": "Configuration Module",
         "description": "Library for loading and saving configuration data.",
         "bugs": null,
@@ -95,25 +95,32 @@
     
             this.then = null;
     
-            this.getURI = function(path) {
-                return mod.uri.parse(root + (path.indexOf("/") == 0? path.substr(1) : path));
+            this.getURI = function(path, ignoreActiveProfile) {
+                return mod.uri.parse(root + (!ignoreActiveProfile? getProfilePath() : "") + (path.indexOf("/") == 0? path.substr(1) : path));
             }
     
-            this.open = function(path, opt_access, create_path) {
-                return mod.uri.open(root + (path.indexOf("/") == 0? path.substr(1) : path), opt_access, create_path);
+            this.open = function(path, opt_access, create_path, ignoreActiveProfile) {
+                return mod.uri.open(root + (!ignoreActiveProfile? getProfilePath() : "") + (path.indexOf("/") == 0? path.substr(1) : path), opt_access, create_path);
             };
     
-            this.exists = function(path) {
-                return mod.uri.exists(root + (path.indexOf("/") == 0? path.substr(1) : path));
+            this.exists = function(path, ignoreActiveProfile) {
+                return mod.uri.exists(root + (!ignoreActiveProfile? getProfilePath() : "") + (path.indexOf("/") == 0? path.substr(1) : path));
             };
     
-            this.query = function(path) {
-                return mod.uri.query(root + (path.indexOf("/") == 0? path.substr(1) : path));
+            this.query = function(path, ignoreActiveProfile) {
+                return mod.uri.query(root + (!ignoreActiveProfile? getProfilePath() : "") + (path.indexOf("/") == 0? path.substr(1) : path));
             };
     
             this.events = new event.Emitter(this);
         };
         ConfigurationVolume.prototype = io.Volume;
+        var profilePath = "";
+        function getProfilePath() {
+            if (!profilePath && typeof allume !== "undefined" && allume.config) {
+                profilePath = "pkx/" + allume.config.activeProfile + "/";
+            }
+            return profilePath;
+        }
         function mountConfigVolume(resolve, reject) {
             //mount config volume if not already mounted
             if (!volume) {
@@ -247,7 +254,7 @@
                 });
             };
     }
-    
+        
     var singleton;
     define(function() {
         if (!singleton) {
